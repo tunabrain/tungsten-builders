a75a66916fcab50e98765e6fba64d6b4c61a9c69
Benedikt Bitterli <mail@noobody.org>
2017-02-12 11:44:21 -0500

    Use ubuntu codename in package version number

--- builders/ubuntu-xenial/build/buildtools/generate-changelog.sh
+++ builders/ubuntu-xenial/build/buildtools/generate-changelog.sh
@@ -1,6 +1,7 @@
 set -eu
+. /etc/lsb-release
 NOW=`date '+%a, %d %b %Y %H:%M:%S +0000'`
-echo "tungsten (${VERSION}) xenial; urgency=low"
+echo "tungsten (${VERSION}~${DISTRIB_CODENAME}1) ${DISTRIB_CODENAME}; urgency=low"
 echo ""
 git log `git describe --tags --abbrev=0`..HEAD --oneline --no-merges --pretty=format:'  * %s'
 echo "\n"
