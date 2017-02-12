29cb06faea7cc58f08a573486acd63c2ff06e466
Benedikt Bitterli <mail@noobody.org>
2017-02-12 11:27:11 -0500

    Remove duplicate command

--- builders/ubuntu-xenial/build/buildtools/generate-changelog.sh
+++ builders/ubuntu-xenial/build/buildtools/generate-changelog.sh
@@ -1,6 +1,5 @@
 set -eu
 NOW=`date '+%a, %d %b %Y %H:%M:%S +0000'`
-DESC=$(git log `git describe --tags --abbrev=0`..HEAD --oneline --no-merges --pretty=format:'  * %s')
 echo "tungsten (${VERSION}) xenial; urgency=low"
 echo ""
 git log `git describe --tags --abbrev=0`..HEAD --oneline --no-merges --pretty=format:'  * %s'
