939fd9c81c0dabdf0a397f76e2c2268e68d8da64
Benedikt Bitterli <mail@noobody.org>
2017-02-12 11:27:26 -0500

    Use dpkg --unpack instead of -i

--- builders/ubuntu-xenial/verify/buildtools/verify.sh
+++ builders/ubuntu-xenial/verify/buildtools/verify.sh
@@ -1,4 +1,4 @@
-sudo dpkg -i /packages/tungsten_${VERSION}_amd64.deb
 set -eu
+sudo dpkg --unpack /packages/tungsten_${VERSION}_amd64.deb
 sudo apt-get install -f -y
 tungsten /usr/share/tungsten/materialtest/materialtest.json -d /verify-output -o tungsten-$VERSION.png
